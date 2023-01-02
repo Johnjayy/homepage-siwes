53c844ffc79eadde5d253ca1ec89e5163644a4e2
Johnjayy <edetjohn638@gmail.com>
2023-01-02 15:37:54 +0100

    second commit

---
+++ test.js
@@ -0,0 +1,313 @@
+var data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>AJINGI LGA</h3> <i>Ajini is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.037864,
+          11.966072
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>ALBASU LGA</h3> <i>Albasu is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          9.140531,
+          11.671703
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>BAGWAI LGA</h3> <i>Bagwai is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.134974,
+          12.157687
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>BEBEJI LGA</h3> <i>Bebeji is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.302479,
+          11.463279
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>BICHI LGA</h3> <i>Bichi is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.315238,
+          12.193559
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>BUNKURE LGA</h3> <i>Bunkure is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.696481,
+          11.635356
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>DAWAKIN KUDU LGA</h3> <i>Dawakin Kudu is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.686887,
+          11.722499
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>GABASAWA LGA</h3> <i>Gabasawa is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.911781,
+          12.175741
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Garun Malam.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>GARUN MALAM LGA</h3> <i>Garun Malam is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.381825,
+          11.616391
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Gezawa.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>GEZAWA LGA</h3> <i>Gezawa is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.631015,
+          12.009357
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>GWARZO LGA</h3> <i>Gwarzo is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.961194,
+          11.842283
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>KARAYE LGA</h3> <i>Karaye is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.977387,
+          11.724309
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>KIBIYA LGA</h3> <i>Kibiya is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.720722,
+          11.565041
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./img/Madobi_LGA.png" alt="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>KIRU LGA</h3> <i>Kiru is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.986117,
+          11.596296
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Kumbotso.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>KUMBOTSO LGA</h3> <i>Kumbotso is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.444637,
+          11.93712
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Kunchi.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>KUNCHI LGA</h3> <i>Kunchi is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.265079,
+          12.502543
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Kura.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>KURA LGA</h3> <i>Kura is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.42709,
+          11.773036
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Makoda.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>MAKODA LGA</h3> <i>Makoda is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.552051,
+          12.338438
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Minjibir.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>MINJIBIR LGA</h3> <i>Minjibir is one of the LGAs of Kano state with about 10 wards and 122 communities.</i>'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.609528,
+          12.105897
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Rano.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>RANO LGA</h3> <i>Rano is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.617683,
+          11.560378
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Rimin Gado.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>RIMIN GADO LGA</h3> <i>Rimin Gado is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.226762,
+          11.966923
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Rogo.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>ROGO LGA</h3> <i>Rogo is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.832778,
+          11.463045
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Shanono.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>SHANONO LGA</h3> <i>Shanono is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.961439,
+          12.108936
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Tofa.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>TOFA LGA</h3> <i>Tofa is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.264661,
+          12.091108
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Tsanyawa.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>TSANYAWA LGA</h3> <i>Tsanyawa is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.101433,
+          12.224654
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Tudun Wada.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>TUDUN WADA LGA</h3> <i>Tudun Wada is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.531592,
+          11.201033
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Ungongo.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>UNGONGO LGA</h3> <i>Ungongo is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.463551,
+          12.064034
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {name: '<img src="./Uko/Uko/LGA_Warawa.png" alt="" srcset="" style="height: 32vh; display: block; margin-left: auto; margin-right: auto;"><h3>WARAWA LGA</h3> <i>Warawa is one of the LGAs of Kano state with about 10 wards and 122 communities.'},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          8.756132,
+          11.91789
+        ]
+      }
+    }
+  ]
+}
